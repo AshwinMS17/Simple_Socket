5995705eed23504396c8b57091eb2dcf905493c0
AshwinMS17 <77296355+AshwinMS17@users.noreply.github.com>
2022-01-22 22:56:47 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 19:07:23 2021
+
+@author: ASHWIN M S
+"""
+
+import socket
+
+run_client = True
+while run_client:
+    c=socket.socket()
+    c.connect(('localhost',9999))
+    str=input('Enter command: ')
+    c.send(bytes(str,'utf-8'))
+    print(c.recv(1024).decode())
+    c.close()
+    choice = input("Enter 'yes' to continue or 'no' to stop client: ")
+    if choice == 'yes':
+        run_client = True
+    else:
+        run_client = False
+
+
+
+
+
